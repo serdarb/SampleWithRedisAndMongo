f9bc8ef9674542b9d14781bd2bc53d2b067541f7
Serdar Büyüktemiz <hserdarb@gmail.com>
2016-04-10 16:57:59 +0300

    first test written... PersonService_Insert_ReturnsPersonInsertResponse

---
+++ Source/Project.Business/PersonService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Project.Common.Models.Request;
+using Project.Common.Models.Response;
+
+namespace Project.Business
+{
+    public class PersonService
+    {
+        public PersonInsertResponse Insert(PersonInsertRequest request)
+        {
+            var response = new PersonInsertResponse();
+
+            return response;
+        }
+    }
+}
--- Test/Project.Business.Test/PersonServiceUnitTests.cs
+++ Test/Project.Business.Test/PersonServiceUnitTests.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+
+using Project.Common.Models.Request;
+using Project.Common.Models.Response;
+using Project.Business;
 
 namespace Project.Business.Test
 {
     public class PersonServiceUnitTests
     {
+        [Fact]
+        public void PersonService_Insert_ReturnsPersonInsertResponse()
+        {
+            //arrange
+            var request = new PersonInsertRequest();
+            var service = new PersonService();
+
+            //act
+            var response = service.Insert(request);
+
+            //assert
+            Assert.IsType<PersonInsertResponse>(response);
+        }
     }
 }
